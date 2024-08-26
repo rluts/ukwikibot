561fea9f361fd55bc080d7dd3af39c3eeb0b3386
rluts <lutsromans@gmail.com>
2024-08-27 01:36:55 +0300

    async running

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import asyncio
 import logging
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from telegram.ext import Application
 
 from wikibot.bot import app
 
@@ -12,5 +17,31 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
+
+async def start(application: Application) -> Application:
+    await application.initialize()
+    await application.updater.start_polling()
+    await application.start()
+    return application
+
+
+async def stop(application: Application) -> None:
+    await application.updater.stop()
+    await application.stop()
+    await application.shutdown()
+
+
+async def run_polling(application: Application) -> None:
+    try:
+        application = await start(application)
+        while application.running:
+            await asyncio.sleep(5)
+        logging.error("Unexpected stopping the bot")
+    except Exception:  # noqa
+        logging.exception(f"Cannot run the bot")
+    finally:
+        await stop(application)
+
+
 if __name__ == "__main__":
-    app.run_polling()
+    asyncio.run(run_polling(app))
