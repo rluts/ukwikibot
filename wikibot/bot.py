8f1bd05ab0a073c5791dfe2a63c2b95935f1b492
rluts <lutsromans@gmail.com>
2024-08-29 14:45:25 +0300

    fix parsing commands

--- wikibot/bot.py
+++ wikibot/bot.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 async def parse_command(message_parser: MessageParser, update: Update, _: ContextTypes.DEFAULT_TYPE):
-    command = update.message.text.lstrip("/").rstrip("@ukwikibot")
+    command = update.message.text.lstrip("/").replace("@ukwikibot", "").lower()
     logger.debug(f"{update.message.text} command")
     if message := await message_parser.get_command_response(command):
         await update.message.reply_text(message, parse_mode=ParseMode.HTML)
